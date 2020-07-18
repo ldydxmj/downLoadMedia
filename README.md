4b65769b1173611b50a5bcd3616ba0293d9dcca9
ldydxmj <mingjin009@126.com>
2020-07-18 16:20:23 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # downLoadMedia
 下载公众号里面的音频
+
+安装 npm i
+
+运行  node index.js
--- index.js
+++ index.js
@@ -1,74 +1,112 @@
 /*
- * @since: 2020-07-15 11:40:59
- * @lastTime: 2020-07-18 14:54:10
+ * @since: 2020-07-18 14:55:09
+ * @lastTime: 2020-07-18 16:18:02
  * @message: 
  */
+"use strict"
+const fs = require("fs");
+const path = require("path");
+// const async = require("async");
+const request = require("request");
+const colors = require("colors");
+const cheerio = require("cheerio");
 
+const opts = {
+    baseUrl: "https://mp.weixin.qq.com/s/dmDSWxvZoU4iytcppH8cyA",
+    medaiBaseUrl: "https://res.wx.qq.com/voice/getvoice?mediaid="
+}
 
-var request = require('request')
-var cheerio = require('cheerio')
-var fs = require('fs')
-var movies = []
-var requstMovie = function (url) {
+class Crawler {
+    constructor() {
 
+    }
+    checkImgPath(p) {
+        try {
+            fs.accessSync(path.join(__dirname, p), fs.F_OK);
+        } catch (e) {
+            fs.mkdirSync(path.join(__dirname, p))
+        }
+    }
 
+    getSongList(url) {
+        const self = this;
+        return new Promise(function (resolve, reject) {
+            request(url, function (err, res, body) {
+                if (!err && res.statusCode == 200) {
+                    let $ = cheerio.load(body)
+                    var as = $('a')
+                    let pageArr = []
+                    //获取pageArr
+                    for (const key in as) {
+                        if (as.hasOwnProperty(key)) {
+                            const element = as[key];
+                            let url = $(element).attr('href')
+                            let title = $(element).children("span").text()
+                            let reg = /http/
 
-    request('https://mp.weixin.qq.com/s/dmDSWxvZoU4iytcppH8cyA', function (error, response, body) {
-        //res.statusCode 为200则表示链接成功
-        if (error === null && response.statusCode === 200) {
-            console.log('链接成功')
-            //使用cheerio来解析body（网页内容），提取我们想要的信息
-            var $ = cheerio.load(body)
-
-            //通过分析网页结构，我们发现豆瓣每部电影都通过item属性隔开
-            var as = $('a')
-            //   var asArr=Array.from(as)
-            let linkArr = []
-            //获取linkArr
-            for (const key in as) {
-                if (as.hasOwnProperty(key)) {
-                    const element = as[key];
-                    let href = $(element).attr('href')
-                    let reg = /http/
-
-                    if (reg.test(href)) {
-                        // console.log("requstMovie -> href", href)
-                        linkArr.push()
+                            if (reg.test(url)) {
+                                pageArr.push({ url, title })
+                            }
+                        }
                     }
+                    resolve(pageArr)
                 }
-            }
-            linkArr = ['https://mp.weixin.qq.com/s/dUVFQprmvGNNJJ1_hwOSeg']
-            linkArr.forEach(function (link) {
+            })
+        })
+    }
+    getSongUrl(page) {
+        return new Promise(function (resolve, reject) {
+            request(page.url, function (err, res, body) {
 
-                request(link, function (error, response, body) {
-                    var mh = cheerio.load(body);
-                    // console.log( mh('.js_editor_audio').attr('voice_encode_fileid'))
+                if (!err && res.statusCode == 200) {
+                    let $ = cheerio.load(body)
+                    var fileId = $('.js_editor_audio').attr('voice_encode_fileid');
+                    var back = opts.medaiBaseUrl
+                    if (fileId) {
+                        back += fileId
+                        resolve(back)
+                    } else {
+                        reject(`${page.title}出错了`)
+                    }
 
-                    var fileId = mh('.js_editor_audio').attr('voice_encode_fileid');
-                    // https://res.wx.qq.com/voice/getvoice?mediaid=MzAxNDcxMjM5NF8yMjQ3NTE4NDg3
-
-                    request("https://res.wx.qq.com/voice/getvoice?mediaid=MzAxNDcxMjM5NF8yMjQ3NTE4NDg3")
-                    .pipe(fs.createWriteStream("123.mp3"))
-                    .on("error",function(err){
-                    console.log("requstMovie -> error")
-                      
-                    })
-                    .on("close",() => {
-                    console.log("requstMovie -> close")
-                       
-                    })
-                  
-
-                })
+                }
+            })
+        })
+    }
+    downloadSong(media) {
+        request(media.url)
+            .pipe(fs.createWriteStream(path.join(__dirname, "media", media.title + ".mp3")))
+            .on("error", function (err) {
+                console.log(`${media.title}下载失败`);
+            })
+            .on("close", () => {
+                console.log(`${media.title}下载成功`);
 
             })
-        }
-    })
+    }
+
+    async start() {
+        this.checkImgPath("media");
+
+        let pageArr = await this.getSongList(opts.baseUrl)
+
+        pageArr.forEach((page) => {
+
+            this.getSongUrl(page).then((url) => {
+                let obj = {
+                    title: page.title,
+                    url
+                }
+                this.downloadSong(obj)
+
+            }, function (e) {
+                console.log(e)
+
+            })
+        })
+    }
 
 }
-requstMovie()
 
-
-var handleAlink = function (a) {
-    console.log(a);
-}
+const crawler = new Crawler();
+crawler.start();
